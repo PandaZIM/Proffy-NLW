74e3ed46619ea47f07565d9ab6b84a101a7d92c8
pandazim <jaotoledo74@gmail.com>
2020-08-05 17:29:08 -0400

    Final commit

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,32 +2,52 @@ import React from 'react'
 
 import './styles.css'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api'
 
-function TeacherItem (){
+export interface Teacher {
+    id: number
+    avatar: string
+    bio: string
+    cost: number
+    name: string
+    subject: string
+    whatsapp: string
+}
+
+export interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) =>{
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return(
         <article className="teacher-item">
         <header>
-            <img src="https://avatars1.githubusercontent.com/u/47374572?s=460&u=e48b8f7de8c4c9e318b0b4182e8892c986e67cf2&v=4" alt="João Pedro Coutinho"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-                <strong>João Pedro Coutinho</strong>
-                <span>Palestrinha</span>
+                <strong>{teacher.name}</strong>
+                <span>{teacher.subject}</span>
             </div>
         </header>
-        <p>
-             Mais de milhares de pessoas ja tiveram de ouvir umas de suas palastras.
-             <br /><br />
-             Variando de politica até jogos e animes.
-        </p>
+        <p>{teacher.bio} </p>
 
         <footer>
             <p>
                 Preço/hora
-                <strong>R$ Arrasta pra cima e descubra</strong>
+                <strong>{teacher.cost}</strong>
             </p>
-            <button type="button">
+            <a 
+            target="_blank"
+            onClick={createNewConnection} 
+            href={`https://wa.me/${teacher.whatsapp}`}>
                 <img src={whatsappIcon} alt="WhatsApp"/>
                 Entrar em contato
-            </button>
+            </a>
         </footer>
     </article>
     )
